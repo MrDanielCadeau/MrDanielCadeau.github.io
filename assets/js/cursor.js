a792d1dfbc1a0fe1e4c559c91d83afb78456d036
Daniel Cadeau <daniel.cadeau13@gmail.com>
2021-03-30 09:37:45 +0200

    little update of the cursor

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,9 +1,10 @@
+let cursor = document.getElementById('cursor');
+
 document.onmousemove = function (e) {
     let x = e.pageX;
     let y = e.pageY;
 
-    let cursor = document.getElementById('cursor');
     cursor.style.display = 'block';
-    cursor.style.left = `${x + 5}px`;
-    cursor.style.top = `${y + 5}px`;
+    cursor.style.left = `${x + 10}px`;
+    cursor.style.top = `${y + 15}px`;
 }
