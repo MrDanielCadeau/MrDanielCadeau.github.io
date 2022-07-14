c250f1325b444b66fc3904fa348570f6a214569b
daniel cadeau <daniel.cadeau13@gmail.com>
2022-07-14 02:04:29 +0200

    removed form

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,31 +12,10 @@
             <a :href="`mailto:${this.profile.email}`"><i class="fas fa-at"></i> {{this.profile.email}}</a>
             <a :href="`tel:${this.profile.phone}`"><i class="fas fa-phone"></i> {{this.profile.phone}}</a>
         </div>
-        <form class="contactForm" @submit.prevent="sendEmail" v-if="!this.res">
-            <label for="user_name">Your Name / Your Company</label>
-            <input id="user_name" type="text" name="user_name" />
-            <label for="user_email">Your email</label>
-            <input id="user_email" type="email" name="user_email" />
-            <label for="message">Your message to me</label>
-            <textarea id="message" name="message"></textarea>
-            <div class="sidebarBottom smoothButtons">
-                <ul>
-                    <li>
-                        <button class="sidebarClose">SEND</button>
-                        <span></span>
-                    </li>
-                </ul>
-            </div>
-        </form>
-        <div v-else class="contactForm">
-            <h3>Thank you for your message, I will try to answer it quickly!</h3>
-        </div>
-        
     </div>
 </template>
 
 <script>
-    import emailjs from 'emailjs-com';
     export default {
         props: {
             profile: Object,
@@ -48,21 +27,6 @@
             }
         },
         methods: {
-            sendEmail: function(e) {
-                emailjs.sendForm('service_6wuwpae', 'template_efwa4ru', e.target, 'user_7XiHEX24HZMuUJUdjIXjH')
-                    .then((result) => {
-                        console.log('SUCCESS!', result.status, result.text)
-                    }, (error) => {
-                        console.log('FAILED...', error)
-                    })
-                emailjs.sendForm('service_6wuwpae', 'template_s44z4w8', e.target, 'user_7XiHEX24HZMuUJUdjIXjH')
-                    .then((result) => {
-                        console.log('SUCCESS!', result.status, result.text)
-                    }, (error) => {
-                        console.log('FAILED...', error)
-                    })
-                this.res = true
-            },
         },
         mounted() {
             console.log('Sidebar is loaded.')
