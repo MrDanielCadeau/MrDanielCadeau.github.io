0438f0cdc5b3ab239b25bad05a901c32ce2b0344
daniel cadeau <daniel.cadeau13@gmail.com>
2022-07-14 02:12:59 +0200

    updated prod branch on deploy sh script

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ git init
 git add -A
 git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:MrDanielCadeau/MrDanielCadeau.github.io.git master
+git push -f git@github.com:MrDanielCadeau/MrDanielCadeau.github.io.git prod
 cd -
