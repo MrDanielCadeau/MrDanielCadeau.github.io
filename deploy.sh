d2226f35478799b43c1ebab91142536b33090068
daniel cadeau <daniel.cadeau13@gmail.com>
2022-07-28 23:10:58 +0200

    remove deploy script, which is overkill in this context

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -e
-git checkout development
-git branch -D prod
-git checkout -b prod
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:MrDanielCadeau/MrDanielCadeau.github.io.git prod
