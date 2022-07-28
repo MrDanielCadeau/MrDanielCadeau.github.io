f6644ac1c1d804bd0d2a672b79301ff07bf80119
daniel cadeau <daniel.cadeau13@gmail.com>
2022-07-28 22:40:46 +0200

    final fix on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -e
+git checkout development
 git delete prod
 git checkout -b prod
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:MrDanielCadeau/MrDanielCadeau.github.io.git prod
-cd -
